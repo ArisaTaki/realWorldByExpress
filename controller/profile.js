b01359d9712c425059bd471f14d5888af6669c90
chenxinglong <xinglong.chen@fenrir-inc.com.cn>
2021-10-18 09:44:33 +0800

    关注与取消关注

--- controller/profile.js
+++ controller/profile.js
@@ -19,7 +19,24 @@ exports.getUserInfo = async (req, res, next) => {
 // 关注用户
 exports.followUser = async (req, res, next) => {
   try {
-    res.send("post /:username/follow");
+
+    const username = req.params.username
+
+    let followProfile = await Profile.findOne({ username })
+
+    if (req.body.following && !followProfile.following) {
+      followProfile.following = true
+      await followProfile.save()
+      res.status(200).json({
+        followProfile
+      });
+    } else {
+      res.status(422).json({
+        errors: {
+          body: "您已关注"
+        }
+      })
+    }
   } catch (err) {
     next(err);
   }
@@ -28,6 +45,24 @@ exports.followUser = async (req, res, next) => {
 // 取消关注
 exports.unFollowUser = async (req, res, next) => {
   try {
+
+    const username = req.params.username
+
+    let followProfile = await Profile.findOne({ username })
+
+    if (!req.body.following && followProfile.following) {
+      followProfile.following = false
+      await followProfile.save()
+      res.status(200).json({
+        followProfile
+      });
+    } else {
+      res.status(422).json({
+        errors: {
+          body: "您未关注"
+        }
+      })
+    }
     res.send("post /:username/follow");
   } catch (err) {
     next(err);
--- router/profile.js
+++ router/profile.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const profileCtrl = require('../controller/profile')
+const auth = require('../middleware/auth')
 
 const router = express.Router()
 
@@ -7,9 +8,9 @@ const router = express.Router()
 router.get('/:username', profileCtrl.getUserInfo)
 
 // 关注用户
-router.post('/:username/follow', profileCtrl.followUser)
+router.post('/:username/follow', auth, profileCtrl.followUser)
 
 // 取消关注
-router.delete('/:username/follow', profileCtrl.unFollowUser)
+router.delete('/:username/follow', auth, profileCtrl.unFollowUser)
 
 module.exports = router
