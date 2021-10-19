7e7be7e266008d0b3d20471bebcd5b3f888ae64d
chenxinglong <xinglong.chen@fenrir-inc.com.cn>
2021-10-19 09:26:38 +0800

    Optimization

--- validator/profile.js
+++ validator/profile.js
@@ -21,7 +21,7 @@ exports.followProfile = validate([
         .custom(async (following, { req }) => {
             // 不能自己关注自己
             if (req.user.username === req.params.username) {
-                return Promise.reject('不能关注自己')
+                return Promise.reject('不能对自己进行操作')
             }
             // 不能重复关注
             const profile = await Profile.findOne({ username: req.params.username })
@@ -37,6 +37,10 @@ exports.unfollowProfile = validate([
         //isBoolean的严格模式还没看到文档，后续再研究，处理布尔值的方法只能使用箭头函数进行下述操作了
         .custom(value => value === false).withMessage('参数非法').bail()
         .custom(async (following, { req }) => {
+            // 不能操作自己
+            if (req.user.username === req.params.username) {
+                return Promise.reject('不能对自己进行操作')
+            }
             // 根本没有关注
             const profile = await Profile.findOne({ username: req.params.username })
             if (!profile.following) {
